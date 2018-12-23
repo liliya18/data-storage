5796f9f48c072162b9c9d0304085c3dda8aad618
Liliya Mannapova <liliya.mf.18@gmail.com>
2018-12-23 20:43:53 +0300

    filter added

--- app/controllers/article_json_controller.rb
+++ app/controllers/article_json_controller.rb
@@ -1,6 +1,14 @@
 class ArticleJsonController < JSONAPI::ResourceController
     def index
-        article = Article.all    
+        article = filter_articles
         render json: article, status: :ok, adapter: :json_api
+    end
+
+    def filter_articles
+        Article.where(
+          "description ILIKE :query OR 
+          title ILIKE :query OR 
+          author ILIKE :query",
+         query: "%#{params[:search]}%").all 
     end
 end
